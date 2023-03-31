594b2ab5f236f85850dc005429a6ff42991e9fd2
Andrii Lisostep <reyandrey@mail.com>
2023-03-31 12:41:43 +0300

    compilated lesson 2

--- app.js
+++ app.js
@@ -207,7 +207,48 @@ if (typeof some === 'string') {
 var person;
 person = ['Max', 21];
 /////////////////////////////////////////////
-var Toggle;
-(function (Toggle) {
-})(Toggle || (Toggle = {}));
-[LOADING, DISABLE];
+var Status;
+(function (Status) {
+    Status[Status["LOADING"] = 0] = "LOADING";
+    Status[Status["READY"] = 1] = "READY";
+})(Status || (Status = {}));
+;
+var page = {
+    load: Status.READY
+};
+if (page.load === Status.LOADING) {
+    console.log('Завантаження');
+}
+if (page.load === Status.READY) {
+    console.log('Завантажено');
+}
+////////////////////////////////////////////////
+var a;
+/////////////////////////////////////////////
+var literal;
+///////////////////////////////////////////////
+function showMessage(message) {
+    console.log(message);
+}
+function calc(num1, num2) {
+    return num1 + num2;
+}
+function customError() {
+    throw new Error('Error');
+}
+var page1 = {
+    title: 'The awesome page',
+    likes: 100,
+    accounts: ['Max', 'Anton', 'Nikita'],
+    status: 'open',
+    details: {
+        createAt: '2021-01-01',
+        updateAt: '2021-05-01'
+    }
+};
+var page2 = {
+    title: 'Python or Js',
+    likes: 5,
+    accounts: ['Alex'],
+    status: 'close'
+};
