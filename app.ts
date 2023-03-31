89e2ad18fadc25d4e4be9f3bd1a8796d1b5db1cb
Andrii Lisostep <reyandrey@mail.com>
2023-03-31 12:37:21 +0300

    lesson 2

--- app.ts
+++ app.ts
@@ -113,4 +113,239 @@
 //     str = some
 // } // type guard
 
-//2.5. Tuple 
+//2.5. Tuple
+
+// let fixed: [string, number];
+
+// fixed = ['str', 1]
+
+// Enum
+
+// enum Toggle [ENABLE, DISABLE];
+
+// const service = [
+//     status: Toggle.ENABLE,
+// ];
+
+// if (service.status === Toggle.ENABLE) {
+//     console.log('It is active')
+// }
+
+//Union Type
+// let union: number | string;
+
+// union = 10;
+// union = "string";
+
+// function combine(param1: string | number, param2: string | number) {
+//     // return param1 + param2; //error
+//     if (typeof param1 === "string" || typeof param2 === "string") {
+//         return param1.toString() + param2.toString()
+//     }
+//     return param1 + param2
+// }
+
+// console.log(combine("str1", "str2"));
+
+//Literal Type
+// let active: "start" | "end";
+
+// active = "start";
+
+//tsc -w app.ts // command mode "watch"
+
+// const fruit: string[] = [];
+
+// function workWithArr(arr, value: string, action: 'add' | "delete"){
+//     if (action === "add") {
+//     arr.push(value)
+//     } else {
+//         const index = arr.indexOf(value)
+//         arr.splice(index, 1)
+//     }
+//     return arr
+// }
+
+// workWithArr(fruit, "banana", "add")
+// workWithArr(fruit, "melon", "add")
+// workWithArr(fruit, "pear", "add")
+// workWithArr(fruit, "Bear", "add")
+
+// workWithArr(fruit, "Bear", "delete")
+// console.log(fruit);
+
+//Return Type
+// function print(): void {
+//     console.log('Some log');
+// }
+// print()
+
+// function combine(num1:number, num2:number): void {
+//     // return num1 + num2 // error
+    
+// }
+
+// function combine(num1:number, num2:number): number {
+//     return num1 + num2
+// }
+
+
+// function customError(): never {
+//    throw new Error("Some error")
+// }
+
+// function createServerPerson(name: string) {
+//     return eval(`
+//     (() => {
+//         return {
+//             name: "${name}"
+//         }
+//     })()
+//     `)
+// }
+
+// function createPerson(name: string): {name: string} {
+//     return createServerPerson(name)
+// }
+
+// const person = createPerson('Alex')
+
+// console.log(person);
+
+//Function Type
+// let foo: (param1: number, param2: string)=>void;
+
+// foo = (param1: number, param2: string) => {
+//     console.log('some text');
+// };
+
+// function culc(num1: number, num2: number, callback: (arg1: number, arg2: number) => number) {
+//     return callback(num1, num2)
+// }
+
+// function foo(num1: number, num2: number) {
+//     return num1 + num2
+// }
+
+// const result = culc(1, 3, foo)
+
+// console.log(result);
+
+// Custom Types
+
+// type PersonType = {
+//     readonly name: string,
+//     age?: number
+//     showName: () => void;
+// };
+// const person1: PersonType = {
+//     name: "Alex",
+//     age: 30,
+//     showName() {
+//         console.log(this.name);
+//     }
+// }
+
+// // person1.name = "New Name" //error, because readonly
+
+// const person2: PersonType = {
+//     name: "Max",
+//      showName() {
+//         console.log(this.name);
+//     }
+// }
+
+// person1.showName();
+// person2.showName()
+
+//PRACTICE
+
+let age: number    
+age = 50;
+let name1: string
+name1 = 'Max';
+let toggle: boolean    
+toggle = true;
+let empty: null;
+empty = null;
+let notInitialize: undefined
+notInitialize = undefined;
+let callback: (a: number) => number;
+callback = (a) => { return 100 + a };
+//////////////////////////////////////////  
+let anything: any;
+anything = -20;
+anything = 'Text';
+anything = {};
+/////////////////////////////////////////
+let some: unknown;
+some = 'Text';
+
+let str: string;
+
+if (typeof some === 'string') {
+str = some;
+}
+/////////////////////////////////////////////
+let person: [string, number]
+person = ['Max', 21];
+/////////////////////////////////////////////
+enum Status { LOADING, READY };
+const page = {
+    load: Status.READY
+}
+if (page.load === Status.LOADING) {
+    console.log('Завантаження');
+}
+if (page.load === Status.READY) {
+    console.log('Завантажено');
+}
+////////////////////////////////////////////////
+let a: string | number
+/////////////////////////////////////////////
+let literal:  'enable' | 'disable'
+///////////////////////////////////////////////
+
+function showMessage(message): void {
+  console.log(message);
+}
+
+
+function calc(num1, num2): number {
+  return num1 + num2;
+}
+
+function customError(): never {
+  throw new Error('Error');
+}
+////////////////////////////////////////////
+
+type Data = {
+    title: string,
+    likes: number,
+    accounts: string[],
+    status: 'open' | 'close',
+    details?: {
+        createAt: string, //Date
+        updateAt: string, //Date
+  }
+}
+
+
+const page1: Data = {
+  title: 'The awesome page',
+  likes: 100,
+  accounts: ['Max', 'Anton', 'Nikita'],
+  status: 'open',
+  details: {
+    createAt: '2021-01-01',
+    updateAt: '2021-05-01',
+  }
+}
+
+const page2 = {
+  title: 'Python or Js',
+  likes: 5,
+  accounts: ['Alex'],
+  status: 'close',
+}
