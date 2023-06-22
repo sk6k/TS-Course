71267b862d81a856b1ea7913d0d61f78ea670aae
Andrii Lisostep <reyandrey@mail.com>
2023-06-22 11:32:56 +0300

    Nulish coalescing

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,5 @@
-// Це спрощення для перевірок, а чи є якесь поле в об'єкті. Можна для цього використовувати Lodash, а можна механізмом в TS.
+const userInput = '';
 
-interface Person {
-	name: string;
-	additionalInfo?: {
-		someInfo: string;
-	};
-}
+const store = userInput ?? 'DEFAULT';
 
-const user: Person = {
-	name: 'Max',
-	additionalInfo: {
-		someInfo: 'player',
-	},
-};
-
-console.log(user?.additionalInfo?.someInfo);
+console.log(store);
