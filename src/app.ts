09c4556e5ddc4914d6d36cdbc5caaada21888f92
Andrii Lisostep <reyandrey@mail.com>
2023-06-22 12:49:47 +0300

    keyof

--- src/app.ts
+++ src/app.ts
@@ -1,29 +1,5 @@
-// function merge<T extends object, U extends object>(objA: T, objB: U) {
-// 	return Object.assign({}, objA, objB);
-// }
-
-// const toMerge1 = {
-// 	name: 'Max',
-// };
-
-// const toMerge2 = {
-// 	age: 21,
-// };
-
-// const merged = merge(toMerge1, toMerge2);
-
-// merged.name;
-
-interface ILength {
-	length: number;
+function extractValue<T extends object, U extends keyof T>(obj: T, key: U) {
+	return obj[key];
 }
 
-function getLength<T extends ILength>(str: T): number {
-	return str.length;
-}
-
-const obj = {
-	length: 10,
-};
-
-getLength(obj);
+const field = extractValue({ name: 'Max' }, 'name');
