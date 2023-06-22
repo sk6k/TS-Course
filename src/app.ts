a65d86eb17a528add4a48d8de2a82585edf96059
Andrii Lisostep <reyandrey@mail.com>
2023-06-22 11:46:55 +0300

    Function overloads

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,8 @@
-const userInput = '';
-
-const store = userInput ?? 'DEFAULT';
-
-console.log(store);
+function add(a: number, b: number): number;
+function add(a: string, b: string): string;
+function add(a: string | number, b: string | number) {
+	if (typeof a === 'string' || typeof b === 'string') {
+		return a.toString() + b.toString();
+	}
+	return a + b;
+}
