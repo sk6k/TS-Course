e5ca60d99fb3c512fb1f343bcbf181e3f24677f2
Andrii Lisostep <reyandrey@mail.com>
2023-06-22 11:27:17 +0300

    Optional Chaining

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,17 @@
-//Використовуються коли заздалегідь невідомо які можуть бутти поля в об'єкті
+// Це спрощення для перевірок, а чи є якесь поле в об'єкті. Можна для цього використовувати Lodash, а можна механізмом в TS.
 
 interface Person {
 	name: string;
-	age: number;
-	[x: string]: string | number;
+	additionalInfo?: {
+		someInfo: string;
+	};
 }
 
 const user: Person = {
 	name: 'Max',
-	gender: 'Man',
-	country: 'USA',
-	age: 30,
+	additionalInfo: {
+		someInfo: 'player',
+	},
 };
+
+console.log(user?.additionalInfo?.someInfo);
