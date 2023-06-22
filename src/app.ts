4740c3defc08b8e2abc63743c554d00ec5b8bcd1
Andrii Lisostep <reyandrey@mail.com>
2023-06-22 11:20:34 +0300

    Index Properties

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,14 @@
-const input = document.getElementById('inputEmail');
+//Використовуються коли заздалегідь невідомо які можуть бутти поля в об'єкті
+
+interface Person {
+	name: string;
+	age: number;
+	[x: string]: string | number;
+}
+
+const user: Person = {
+	name: 'Max',
+	gender: 'Man',
+	country: 'USA',
+	age: 30,
+};
